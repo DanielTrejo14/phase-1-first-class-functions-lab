adfad4b5d99c071eb439b90477e0e8eaf412edca
Daniel Trejo <dwtrejo14@comcast.net>
2024-05-15 10:27:12 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+  
+
+  function createFareMultiplier(multiplier) {
+    return function(value) {
+      return value * multiplier;
+    };
+  }
+
+  function fareDoubler(fares) {
+    return fares * 2
+  }
+
+  function fareTripler(fares) {
+    return fares * 3
+  }
+
+function selectDifferentDrivers(drivers, driverSelector) {
+    return driverSelector(drivers);
+  }
